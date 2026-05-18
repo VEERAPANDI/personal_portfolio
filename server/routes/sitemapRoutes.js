e4e5957ec930f64cb1f78ba0ce7c56fe043753de
Veerapandi Lakshmanan <veerapandi.pixelweb@gmail.com>
2026-05-18 13:29:39 +0530

    fix: update sitemap XML structure and change frequency for blog URLs

--- server/routes/sitemapRoutes.js
+++ server/routes/sitemapRoutes.js
@@ -10,23 +10,23 @@ router.get('/sitemap.xml', async (req, res) => {
     let xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>${baseUrl}/</loc>
+    <loc>${baseUrl}</loc>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
-    <loc>${baseUrl}/blog</loc>
-    <changefreq>daily</changefreq>
+    <loc>${baseUrl}blog</loc>
+    <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>`;
 
     blogs.forEach(blog => {
       xml += `
   <url>
-    <loc>${baseUrl}/blog/${blog.slug}</loc>
+    <loc>${baseUrl}blog/${blog.slug}</loc>
     <lastmod>${new Date(blog.updatedAt).toISOString()}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.6</priority>
+    <changefreq>weekly</changefreq>
+    <priority>0.9</priority>
   </url>`;
     });
 
